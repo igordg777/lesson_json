b40d87682a0b2c450c8b7b5ec0c5dea1a5907a1a
itrooms <poverum@bk.ru>
2023-06-17 03:51:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  let obj = {
+    text: `Приветики`,
+    tts: `<speaker audio="alice-music-harp-1.opus">. Приветики`,
+    buttons: [
+      { title: 'Начинаем', hide: true },
+    ],
+    end_session: false
+  };
+  res.json({obj})
+})
+
+app.listen(3000)
